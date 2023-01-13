b29161aa24163037b97c6a5454bf869273353eb8
Celil Aydın <m.celilaydin@gmail.com>
2023-01-13 04:31:57 +0300

    added DetailVC

---
+++ OrderApp/View/controller/Detail/DetailVC.swift
@@ -0,0 +1,26 @@
+//
+//  DetailVC.swift
+//  OrderApp
+//
+//  Created by Celil Aydın on 13.01.2023.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+    
+    @IBOutlet weak var detailImageVİew: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
